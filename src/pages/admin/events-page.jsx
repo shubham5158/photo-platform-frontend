6b8c1910e4f6b38d92f39b36e86ac46a6eb4b398
Shubham <shubhamgogawale1@gmail.com>
2025-12-23 16:24:44 +0530

    bug

--- src/pages/admin/events-page.jsx
+++ src/pages/admin/events-page.jsx
@@ -343,7 +343,7 @@ const EventsPage = () => {
 
             <div className="flex justify-end gap-3 mt-5">
               <button
-                onClick={() => setDeleteEvent(null)}
+                onClick={() => setCreating(null)}
                 disabled={saving}
                 className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50"
               >
