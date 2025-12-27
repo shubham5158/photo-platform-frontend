3157e863848b8b98c30fc9d00767095b7576d870
Shubham <shubhamgogawale1@gmail.com>
2025-12-27 12:27:58 +0530

    fix

--- src/pages/client/client-landing-page.jsx
+++ src/pages/client/client-landing-page.jsx
@@ -8,7 +8,7 @@ import {
   Instagram,
   Mail,
 } from "lucide-react";
-import Button from "@/components/ui/Button";
+import Button from "../../components/ui/Button.jsx";
 
 const HomePage = () => {
   const scrollTo = (e, id) => {
